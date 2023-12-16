db73b931c086b4d7bd41003f95ba5dfbf2d099d9
Arjios <antonio.uab.ios.fisica@gmail.com>
2023-12-16 18:54:05 -0300

    First commit

---
+++ backend/src/main/java/com/arjios/cabanas/CabanasApplication.java
@@ -0,0 +1,13 @@
+package com.arjios.cabanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabanasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/arjios/cabanas/CabanasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjios.cabanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
