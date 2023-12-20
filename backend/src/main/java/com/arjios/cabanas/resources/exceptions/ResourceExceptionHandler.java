3a6739de89e73450d5cd15a54ec477c6a608ad2b
Arjios <antonio.uab.ios.fisica@gmail.com>
2023-12-20 20:14:07 -0300

    FindById and Exception Handler

---
+++ backend/src/main/java/com/arjios/cabanas/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.arjios.cabanas.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.arjios.cabanas.services.exceptions.EntityNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> EntityNotFound(EntityNotFoundException enfe,
+			HttpServletRequest request) {
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setError("Resource Not Found");
+		error.setMessage(enfe.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+}
--- backend/src/main/java/com/arjios/cabanas/services/CategoryService.java
+++ backend/src/main/java/com/arjios/cabanas/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.arjios.cabanas.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.arjios.cabanas.dto.CategoryDTO;
 import com.arjios.cabanas.entities.Category;
 import com.arjios.cabanas.repositories.CategoryRepository;
+import com.arjios.cabanas.services.exceptions.EntityNotFoundException;
 
 import jakarta.transaction.Transactional;
 
@@ -22,5 +24,12 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
 		return list.stream().map(c -> new CategoryDTO(c)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Erro: Entidade não encontrada"));
+		return new CategoryDTO(entity);
 	}
 }
